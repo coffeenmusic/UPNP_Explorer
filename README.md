8836a70e8fd31c974e19c909f5ed2ec0a1197221
Stephen Thompson <stephen.revel.thompson@gmail.com>
2020-07-15 15:01:07 -0400

    code cleanup and README added

---
+++ README.md
@@ -0,0 +1,87 @@
+# References
+- ["Exploring UPnP with Python" - Electric Monk](https://www.electricmonk.nl/log/2016/07/05/exploring-upnp-with-python/)
+  - A lot of the SSDP code is a direct copy from this reference
+- [upnpclient](https://github.com/flyte/upnpclient/blob/develop/upnpclient/soap.py)
+  - A lot of the SOAP request code is a combination of this reference and the first reference
+  
+# Motivation
+I just wanted to play around with UPNP device discovery. I'm sure you can find better code out there, but I do like the way I condensed down the Action and Argument definitions so it is really easy to see what is available.
+
+Look at the Example.ipynb to see the commands necessary. The jupyter notebook is more descriptive than the README overview below.
+
+# Quick Overview
+Step 1: Broadcast
+```
+upnp = UPNP_Explorer() # Broadcast to UPNP Devices
+upnp.SSDP.print_servers()
+```
+>Samsung-Linux/4.1, UPnP/1.0, Samsung_UPnP_SDK/1.0_0
+http/1.1 200 ok: HTTP/1.1 200 OK
+cache-control: max-age=1800
+date: Wed, 15 Jul 2020 18:01:31 GMT
+location: http://192.168.1.14:9197/dmr
+st: upnp:rootdevice
+usn: uuid:036cb031-f5f8-4831-bcd7-c0259ee25740::upnp:rootdevice
+content-length: 0
+bootid.upnp.org: 4
+	
+Step 2: Define Server
+```
+url_list = upnp.define_server('samsung')
+```
+		
+Step 3a: Choose URL Set (Devices can have more than one set of URLs)
+```
+USE_IDX = 0
+CTRL_URL = upnp.SCPD.url_list[USE_IDX]['ctrl']
+SCPD_URL = upnp.SCPD.url_list[USE_IDX]['scpd']
+SERVICE_TYPE = upnp.SCPD.url_list[USE_IDX]['type']
+```
+Step 3b: Get the Service XML
+```
+upnp.SCPD.get_service_xml(SCPD_URL)
+```
+Step 4a: Define Actions and Input Arguments
+```
+upnp.SCPD.pretty_print_actions()
+```
+>    ListPresets(InstanceID)   
+&nbsp;&nbsp;&nbsp;&nbsp;Returns: (CurrentPresetNameList)
+SelectPreset(InstanceID, PresetName)
+GetMute(InstanceID, Channel)
+&nbsp;&nbsp;&nbsp;&nbsp;Returns: (CurrentMute)
+SetMute(InstanceID, Channel, DesiredMute)
+GetVolume(InstanceID, Channel)
+&nbsp;&nbsp;&nbsp;&nbsp;Returns: (CurrentVolume)
+    SetVolume(InstanceID, Channel, DesiredVolume)
+```
+upnp.SCPD.print_action_arg_desc(ACTION_NAME)
+```
+>InstanceID
+&nbsp;&nbsp;&nbsp;&nbsp;dataType: ui4
+Channel
+&nbsp;&nbsp;&nbsp;&nbsp;dataType: string
+&nbsp;&nbsp;&nbsp;&nbsp;allowedValueList: Master
+DesiredMute
+&nbsp;&nbsp;&nbsp;&nbsp;dataType: boolean
+```
+argument_inputs = upnp.SCPD.define_action('SetMute')
+print(upnp.SCPD.input_args)
+```
+>{'InstanceID': 0, 'Channel': 'Master', 'DesiredMute': None}
+	
+Step 4b: Change any input action values if necessary or desired
+```
+argument_inputs['DesiredMute'] = 1
+```
+Step 5: 
+```
+upnp.request_action(CTRL_URL, SERVICE_TYPE) # ACTION_NAME & INPUT_ARGS defined from SCPD.define_action
+upnp.print_xml(upnp.SOAP.response)
+```
+><?xml version="1.0" ?>
+<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+	<s:Body>
+		<u:SetMuteResponse xmlns:u="urn:schemas-upnp-org:service:RenderingControl:1"/>
+	</s:Body>
+</s:Envelope>
--- upnp.py
+++ upnp.py
@@ -5,89 +5,6 @@ import requests
 from urllib.parse import urljoin
 import xml.dom.minidom
 
-class UPNP_SSDP:
-    UPNP_BROADCAST_PORT = 1900
-    UPNP_BROADCAST_IP = '239.255.255.250'
-    UPNP_BRADCAST_MSG = \
-            'M-SEARCH * HTTP/1.1\r\n' \
-            'HOST:239.255.255.250:1900\r\n' \
-            'ST:upnp:rootdevice\r\n' \
-            'MX:2\r\n' \
-            'MAN:"ssdp:discover"\r\n' \
-            '\r\n'
-    
-    def __init__(self, print_broadcast=False):
-        self.print_broadcast = print_broadcast
-        self.broadcast()
-        
-    def broadcast(self, socket_timeout=2):
-        # Set up UDP socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        s.settimeout(socket_timeout)
-        s.sendto(str.encode(self.UPNP_BRADCAST_MSG), (self.UPNP_BROADCAST_IP, self.UPNP_BROADCAST_PORT))
-        
-        self.servers = {}
-        try:
-            while True:
-                data, addr = s.recvfrom(4096)
-                server, property_dict = self.get_response_dict(data.decode())
-
-                server += '_0'
-                if server in self.servers.keys():
-                    cnt = int(server.split('_')[-1]) + 1
-                    server = server[:-len(str(cnt-1))] + str(cnt)        
-
-                self.servers[server] = property_dict
-                if self.print_broadcast:
-                    print(addr)
-                    print(data.decode())
-        except socket.timeout:
-            pass
-        
-    def get_response_dict(self, data_str):
-        property_dict = {}
-        server = ''
-        for line in data_str.split('\r\n'):
-            if line.lower().startswith('server:'):
-                server = line[len('server: '):]
-            else:
-                k = line.split(': ')[0].lower()
-                v = line.split(': ')[-1]
-
-                if k != '' and v != '':
-                    property_dict[k] = v
-
-        return server, property_dict
-    
-    """
-    - server_name: server name does not have to match exacty, but the entire search str must exist in servers' key
-    - servers: dictionary with key: server names, value: property_dict
-    - server_idx: if more than one server are found with server_name, then the index is used to pick which server
-    
-    returns all properties for given server as property dictionary
-    """
-    def get_property_dict(self, server_name, server_idx=0):
-        assert len(self.servers) > 0, 'Error: servers dictionary empty.'
-        server_key = [k for k in self.servers.keys() if server_name.lower() in k.lower()]
-        assert len(server_key) > 0, 'Error: No server with specified key word found.'
-        if len(server_key) > 1:
-            print('Multiple servers found for server name (%s). Please use server_idx input argument to specify which server ' \
-                  'to use or be more specific when specifying server_name. %s index currently used.'%(server_name, server_idx))              
-            print('Servers found:\r\n', ''.join(['\t%s\r\n'%(s) for s in server_key]))
-        
-        if len(server_key) >= server_idx:
-            server_idx = 0
-        server_key = server_key[server_idx]
-
-        return self.servers[server_key]
-    
-    def print_servers(self):
-        for server, values in self.servers.items():
-            print(server)
-            for k, v in values.items():
-                print('\t%s: %s'%(k,v))
-            print()
-
 class UPNP_Explorer:
     def __init__(self):
         # Broadcast to UPNP Servers & Get Response
@@ -127,7 +44,97 @@ class UPNP_Explorer:
 
     def print_xml(self, xml_str):
         print(xml.dom.minidom.parseString(xml_str).toprettyxml())
+
+
+class UPNP_SSDP:
+    UPNP_BROADCAST_PORT = 1900
+    UPNP_BROADCAST_IP = '239.255.255.250'
+    UPNP_BRADCAST_MSG = \
+            'M-SEARCH * HTTP/1.1\r\n' \
+            'HOST:239.255.255.250:1900\r\n' \
+            'ST:upnp:rootdevice\r\n' \
+            'MX:2\r\n' \
+            'MAN:"ssdp:discover"\r\n' \
+            '\r\n'
+    
+    def __init__(self, print_broadcast=False):
+        self.print_broadcast = print_broadcast
+        self.broadcast()
         
+    def broadcast(self, socket_timeout=2, buffer_size=4096):
+        # Set up UDP socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        s.settimeout(socket_timeout)
+        s.sendto(str.encode(self.UPNP_BRADCAST_MSG), (self.UPNP_BROADCAST_IP, self.UPNP_BROADCAST_PORT))
+        
+        self.servers = {}
+        try:
+            while True:
+                data, addr = s.recvfrom(buffer_size)
+                server, property_dict = self.get_response_dict(data.decode())
+
+                server += '_0'
+                if server in self.servers.keys():
+                    cnt = int(server.split('_')[-1]) + 1
+                    server = server[:-len(str(cnt-1))] + str(cnt)        
+
+                self.servers[server] = property_dict
+                if self.print_broadcast:
+                    print(addr)
+                    print(data.decode())
+        except socket.timeout:
+            pass
+        
+    def get_response_dict(self, data_str):
+		"""
+		Formats SSDP Response from UPNP Devices to a dictionary of server properties
+		- data_str: device response
+		
+		return server_name, response_dict
+		"""
+	
+        property_dict = {}
+        server = ''
+        for line in data_str.split('\r\n'):
+            if line.lower().startswith('server:'):
+                server = line[len('server: '):]
+            else:
+                k = line.split(': ')[0].lower()
+                v = line.split(': ')[-1]
+
+                if k != '' and v != '':
+                    property_dict[k] = v
+
+        return server, property_dict
+    
+    def get_property_dict(self, server_name, server_idx=0):
+	    """
+		- server_name: server name does not have to match exacty, but the entire search str must exist in servers' key
+		- server_idx: if more than one server are found with server_name, then the index is used to pick which server
+		
+		returns all properties for given server as property dictionary
+		"""
+	
+        assert len(self.servers) > 0, 'Error: servers dictionary empty.'
+        server_key = [k for k in self.servers.keys() if server_name.lower() in k.lower()]
+        assert len(server_key) > 0, 'Error: No server with specified key word found.'
+        if len(server_key) > 1:
+            print('Multiple servers found for server name (%s). Please use server_idx input argument to specify which server ' \
+                  'to use or be more specific when specifying server_name. %s index currently used.'%(server_name, server_idx))              
+            print('Servers found:\r\n', ''.join(['\t%s\r\n'%(s) for s in server_key]))
+        
+        if len(server_key) >= server_idx:
+            server_idx = 0
+        server_key = server_key[server_idx]
+
+        return self.servers[server_key]
+    
+    def print_servers(self):
+        for server, values in self.servers.items():
+            print(server)
+            for k, v in values.items():
+                print('\t%s: %s'%(k,v))
+            print()        
         
 class UPNP_SCPD:
     def __init__(self, server_name, location_url):
@@ -148,7 +155,7 @@ class UPNP_SCPD:
         
     def get_service_urls(self):
         # Get Base URL
-        urlbase = self._scpd_filter(self.root, filter_tag='urlbase')
+        urlbase = self._filter_etree(self.root, filter_tag='urlbase')
         if len(urlbase) >= 1:
             self.url_soap_req_base = urlbase[0].text
         else:
@@ -157,9 +164,9 @@ class UPNP_SCPD:
         burl = self.url_soap_req_base
         
         # Get Service URLs    
-        ctrl = self._scpd_filter(self.root, filter_prt_tag='service', filter_tag='controlurl')
-        scpd = self._scpd_filter(self.root, filter_prt_tag='service', filter_tag='scpdurl')
-        stype = self._scpd_filter(self.root, filter_prt_tag='service', filter_tag='servicetype')
+        ctrl = self._filter_etree(self.root, filter_prt_tag='service', filter_tag='controlurl')
+        scpd = self._filter_etree(self.root, filter_prt_tag='service', filter_tag='scpdurl')
+        stype = self._filter_etree(self.root, filter_prt_tag='service', filter_tag='servicetype')
 
         self.url_list = []
         for c, s, st in zip(ctrl, scpd, stype):
@@ -167,7 +174,19 @@ class UPNP_SCPD:
             self.url_list += [url_dict]      
         return self.url_list
     
-    def _scpd_filter(self, obj, results=None, filter_txt='', filter_tag='', filter_prt_tag='', exact=False):
+    def _filter_etree(self, obj, filter_txt='', filter_tag='', filter_prt_tag='', exact=False, results=None):
+		"""
+		Input an etree element and return that element filtering the tag, parent tag, and tag's associated text value
+		- obj: Element tree object created from xml
+		- filter_txt: gets objects that contain or match this text
+		- filter_tag: gets objects that contain or match this tag
+		- filter_prt_tag: gets objects that contain or match the parent tag
+		- exact: False=filter string is in element value/tag, True=filter string exactly matches element value/tag
+		- results: a list of filtered element objects. Only used for recursion not used outside that.
+		
+		return results: list of elements that match filter settings
+		"""
+	
         if results == None:
             results = []
 
@@ -182,26 +201,30 @@ class UPNP_SCPD:
                 if filter_txt.lower() in v_str.lower() and filter_tag.lower() in t_str.lower() and filter_prt_tag.lower() in p_str.lower():
                     results += [v]
 
-            self._scpd_filter(v, results=results, filter_txt=filter_txt, filter_tag=filter_tag, filter_prt_tag=filter_prt_tag, exact=exact)
+            self._filter_etree(v, results=results, filter_txt=filter_txt, filter_tag=filter_tag, filter_prt_tag=filter_prt_tag, exact=exact)
 
         return results
     
-    """
-        - arg_dir: None, 'in', 'out'
-    """
     def get_action_arguments(self, action_name, arg_dir=None):
-        action = self._scpd_filter(self.service_root, filter_tag='name', filter_prt_tag='action', filter_txt=action_name)
+		"""
+			- action_name: action that you would like all arguments for
+			- arg_dir: None, 'in', 'out'
+			
+			return arguments associated with action name that are either inputs, outputs, or both depending on arg_dir
+		"""
+	
+        action = self._filter_etree(self.service_root, filter_tag='name', filter_prt_tag='action', filter_txt=action_name)
         if len(action) == 0:
             return
 
         action = action[0].getparent()
-        arguments = self._scpd_filter(action, filter_prt_tag='argument', filter_tag='name')
+        arguments = self._filter_etree(action, filter_prt_tag='argument', filter_tag='name')
         if arg_dir == None:
             return arguments
 
         # Get arguments only for specified direction
         filtered_args = []
-        directions = self._scpd_filter(action, filter_prt_tag='argument', filter_tag='direction')
+        directions = self._filter_etree(action, filter_prt_tag='argument', filter_tag='direction')
         for a, d in zip(arguments, directions):
             if d.text == arg_dir:
                 filtered_args += [a]
@@ -209,20 +232,20 @@ class UPNP_SCPD:
         return filtered_args
         
     def pretty_print_actions(self, name_filter='', show_datatype=False): 
-        actionlist = self._scpd_filter(self.service_root, filter_tag='actionlist')[0]
-        actions = self._scpd_filter(actionlist, filter_tag='action')
+        actionlist = self._filter_etree(self.service_root, filter_tag='actionlist')[0]
+        actions = self._filter_etree(actionlist, filter_tag='action')
         for action in actions:
-            name = self._scpd_filter(action, filter_tag='name')[0].text
+            name = self._filter_etree(action, filter_tag='name')[0].text
             if name_filter.lower() not in name.lower():
                 continue
 
-            arguments = self._scpd_filter(action, filter_tag='argument', filter_prt_tag='argumentlist')
+            arguments = self._filter_etree(action, filter_tag='argument', filter_prt_tag='argumentlist')
 
             print_str = name + '('
             in_args, out_args = [], []
             for arg in arguments:
-                aname = self._scpd_filter(arg, filter_tag='name', filter_prt_tag='argument')[0].text
-                adir = self._scpd_filter(arg, filter_tag='direction', filter_prt_tag='argument')[0].text
+                aname = self._filter_etree(arg, filter_tag='name', filter_prt_tag='argument')[0].text
+                adir = self._filter_etree(arg, filter_tag='direction', filter_prt_tag='argument')[0].text
 
                 desc_dict = self.get_statevar_desc(name, aname)
                 dt = '[%s]'%(desc_dict['dataType']) if 'dataType' in desc_dict and show_datatype else ''
@@ -237,17 +260,17 @@ class UPNP_SCPD:
             print(print_str)
             
     def get_relatedstatevariable(self, action_name, arg_name):
-        action = self._scpd_filter(self.service_root, filter_prt_tag='action', filter_tag='name', filter_txt=action_name)
+        action = self._filter_etree(self.service_root, filter_prt_tag='action', filter_tag='name', filter_txt=action_name)
         assert len(action) > 0, '(%s) action not found'%(action_name)
         action = action[0].getparent()
-        argument = self._scpd_filter(action, filter_prt_tag='argument', filter_tag='name', filter_txt=arg_name)
+        argument = self._filter_etree(action, filter_prt_tag='argument', filter_tag='name', filter_txt=arg_name)
         assert len(argument) > 0, '(%s) argument not found'%(arg_name)
         argument = argument[0].getparent()
-        return self._scpd_filter(argument, filter_tag='relatedstatevariable')[0].text
+        return self._filter_etree(argument, filter_tag='relatedstatevariable')[0].text
 
     def get_statevar_desc(self, action_name, arg_name, show_id=False):
         rsv = self.get_relatedstatevariable(action_name, arg_name)
-        statevar = self._scpd_filter(self.service_root, filter_prt_tag='statevariable', filter_tag='name', filter_txt=rsv, exact=True)[0].getparent()
+        statevar = self._filter_etree(self.service_root, filter_prt_tag='statevariable', filter_tag='name', filter_txt=rsv, exact=True)[0].getparent()
 
         tags = {}
         for sv_tag in statevar.getchildren():
